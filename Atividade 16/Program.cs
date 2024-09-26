8285ff0da87b19be31e2c037947932f49f1cdaa6
PauleraProg <pdietrichfilho1@gmail.com>
2024-09-25 21:34:51 -0400

    Update Program.cs

--- Atividade 16/Program.cs
+++ Atividade 16/Program.cs
@@ -5,7 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Determine os pontos de duas variáveis em um plano: ");
-            double[]
+            Console.Write("X: "); List<double> X = Console.ReadLine().Split().Select(double.Parse).ToList();
+            Console.Write("Y: "); List<double> Y = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+            Console.WriteLine(Dist(X[0], X[1], Y[0], Y[1]));
+
+            static string Dist(double A, double B, double C, double D) 
+            {
+                double distancia = Math.Sqrt(Math.Pow((B - A), 2) + Math.Pow((D - C), 2));
+                return distancia.ToString("F4");
+            }
         }
     }
 }
